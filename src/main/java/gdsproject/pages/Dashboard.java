05809c5a3a9bf27f5f1a3a4b80fe4750e4fcf791
Sridevi <vishnubhotlasridevi@gmail.com>
2021-09-18 09:14:18 +0800

    Sridevi latest commit

--- src/main/java/gdsproject/pages/Dashboard.java
+++ src/main/java/gdsproject/pages/Dashboard.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.By;
 public class Dashboard extends Login {
 
     public void getNewGrant(){
-        /*try {
+        try {
             Thread.sleep(15000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
         //Util.waitForPageLoad(driver, By.xpath("//*[text()='Get new grant']"), 30l);
         driver.findElement(By.xpath("//*[text()='Get new grant']")).click();
     }
